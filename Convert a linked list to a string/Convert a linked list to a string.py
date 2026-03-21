e0be39812268035aa7680224a13d049ff20101f1
sonyarubakha <sonya.rubakha@gmail.com>
2026-03-21 22:11:12 +0200

    add docstrings

--- Convert a linked list to a string/Convert a linked list to a string.py
+++ Convert a linked list to a string/Convert a linked list to a string.py
@@ -1,4 +1,8 @@
+'''Convert a linked list to a string kata'''
 def stringify(node):
+    '''
+    Converts a linked list to a string with data.
+    '''
     res = []
     while node is not None:
         res.append(str(node.data))
