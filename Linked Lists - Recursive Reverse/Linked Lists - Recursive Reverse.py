b07b6c6be9d33fd8e2c3d70f001259d5fd68022a
sonyarubakha <sonya.rubakha@gmail.com>
2026-03-22 20:47:05 +0200

    add docstrings

--- Linked Lists - Recursive Reverse/Linked Lists - Recursive Reverse.py
+++ Linked Lists - Recursive Reverse/Linked Lists - Recursive Reverse.py
@@ -1,9 +1,16 @@
+'''Linked Lists - Recursive Reverse kata'''
 class Node(object):
+    '''
+    Node class.
+    '''
     def __init__(self, data=None):
         self.data = data
         self.next = None
 
 def reverse(head):
+    '''
+    recursively reverses a linked list
+    '''
     if not head or not head.next:
         return head
     new = reverse(head.next)
