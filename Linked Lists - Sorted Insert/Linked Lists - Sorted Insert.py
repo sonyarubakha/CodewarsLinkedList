a2a278a4c144554c2899574c7e9c93bc2ded47e9
sonyarubakha <sonya.rubakha@gmail.com>
2026-03-22 20:24:47 +0200

    add docstrings

--- Linked Lists - Sorted Insert/Linked Lists - Sorted Insert.py
+++ Linked Lists - Sorted Insert/Linked Lists - Sorted Insert.py
@@ -1,11 +1,20 @@
-
+'''Linked Lists - Sorted Insert'''
 class Node(object):
+    '''
+    Node class.
+    '''
     def __init__(self, data):
+        '''
+        Initializes the node.
+        '''
         self.data = data
         self.next = None
 
 
 def sorted_insert(head, data):
+    '''
+    Inserts new node but in a sorted place.
+    '''
     new = Node(data)
     if not head or new.data < head.data:
         new.next = head
