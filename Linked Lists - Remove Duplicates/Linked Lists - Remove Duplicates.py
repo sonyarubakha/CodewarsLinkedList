727c72e22b30b1b4d15603192d9afe4b2fbf2516
sonyarubakha <sonya.rubakha@gmail.com>
2026-03-22 21:26:03 +0200

    add docstrings

--- Linked Lists - Remove Duplicates/Linked Lists - Remove Duplicates.py
+++ Linked Lists - Remove Duplicates/Linked Lists - Remove Duplicates.py
@@ -1,9 +1,19 @@
+'''Linked Lists - Remove Duplicates kata'''
 class Node(object):
+    '''
+    Node class.
+    '''
     def __init__(self, data):
+        '''
+        Initializes the node
+        '''
         self.data = data
         self.next = None
 
 def remove_duplicates(head):
+    '''
+    removes duplicates from linked lists.
+    '''
     if not head:
         return head
     now = head
