e6041b1f3031d86d2b5e6ae3b085f1ac7fe168fb
sonyarubakha <sonya.rubakha@gmail.com>
2026-03-22 12:15:34 +0200

    add docstrings

--- Linked Lists - Push & BuildOneTwoThree/Linked Lists - Push & BuildOneTwoThree.py
+++ Linked Lists - Push & BuildOneTwoThree/Linked Lists - Push & BuildOneTwoThree.py
@@ -1,16 +1,28 @@
-
+'''Linked Lists - Push & BuildOneTwoThree kata'''
 class Node:
+    '''
+    Node class.
+    '''
     def __init__(self, data):
+        '''
+        Initializes the node.
+        '''
         self.data = data
         self.next = None
 
 
 def push(head, data):
+    '''
+    Adds new node on the beginning of linked list.
+    '''
     new = Node(data)
     new.next = head
     return new
 
 def build_one_two_three():
+    '''
+    Creates and returns a linked list like 1 -> 2 -> 3 -> None
+    '''
     res = None
     res = push(res, 3)
     res = push(res, 2)
