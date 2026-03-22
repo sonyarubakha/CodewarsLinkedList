29ef3038eef8238b50ed8dacd2d4473a9fee19b3
sonyarubakha <sonya.rubakha@gmail.com>
2026-03-22 14:24:12 +0200

    add docstring

--- Linked Lists - Move Node/Linked Lists - Move Node.py
+++ Linked Lists - Move Node/Linked Lists - Move Node.py
@@ -1,14 +1,31 @@
+'''Linked Lists - Move Node kata'''
 class Node(object):
+    '''
+    Node class.
+    '''
     def __init__(self, data):
+        '''
+        Initializes the node.
+        '''
         self.data = data
         self.next = None
 
 class Context(object):
+    '''
+    Context class.
+    '''
     def __init__(self, source, dest):
+        '''
+        Initializes the context object.
+        '''
         self.source = source
         self.dest = dest
 
 def move_node(source, dest):
+    '''
+    takes the node from the front of the source list
+    and moves it to the front of the destintation list.
+    '''
     if not source:
         raise ValueError
     move = source
