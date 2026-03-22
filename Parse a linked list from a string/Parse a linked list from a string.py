f7e767e28458a0980e32b657e1e52cadf2643dbc
sonyarubakha <sonya.rubakha@gmail.com>
2026-03-22 11:38:59 +0200

    add docstrings

--- Parse a linked list from a string/Parse a linked list from a string.py
+++ Parse a linked list from a string/Parse a linked list from a string.py
@@ -1,9 +1,19 @@
+'''Parse a linked list from a string kata'''
 class Node:
+    '''
+    Node class.
+    '''
     def __init__(self, data, next=None):
+        '''
+        Initializes the node.
+        '''
         self.data = data
         self.next = next
 
 def linked_list_from_string(list_repr: str) -> Node | None:
+    '''
+    Parses a linked list from a string.
+    '''
     info = list_repr.split(' -> ')
     info = info[:-1]
     if not info:
