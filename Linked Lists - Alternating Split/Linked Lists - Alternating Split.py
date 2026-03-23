3433c67951978c7b2e1f55563d90395a01028d0c
sonyarubakha <sonya.rubakha@gmail.com>
2026-03-23 08:14:18 +0200

    add docstrings

--- Linked Lists - Alternating Split/Linked Lists - Alternating Split.py
+++ Linked Lists - Alternating Split/Linked Lists - Alternating Split.py
@@ -1,14 +1,29 @@
+'''Linked Lists - Alternating Split'''
 class Node(object):
+    '''
+    Node class.
+    '''
     def __init__(self, data=None):
         self.data = data
         self.next = None
 
 class Context(object):
+    '''
+    Context object class.
+    '''
     def __init__(self, first, second):
+        '''
+        Initializes object that helps to keep info
+        about nodes.
+
+        '''
         self.first = first
         self.second = second
 
 def alternating_split(head):
+    '''
+    takes one list and divides up its nodes to make two smaller lists
+    '''
     if not head or not head.next:
         raise ValueError('The list is too short.')
     first = head
